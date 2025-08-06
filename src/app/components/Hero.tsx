d77b0c8031ea23f55301a37c09df898d4c1d0117
Meeth Patel <meeth2060@gmail.com>
2025-08-06 16:28:59 +0530

    hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 function HeroSection() {
   return (
     <div>
-      <div className='flex items-center jutify-between gap-x-3'>
-      <Image src={"/boards4.jpg"}  width={300} alt='board' height={300} />
-      <Image src={"/phone.png"} width={300} alt='phone' height={300} className='rounded-xl'  />
-       <Image src={"/board1.jpg"} width={300} alt='board' height={300}/>
-           </div>
+      <div className="flex items-center jutify-between gap-x-3">
+        <Image src={"/boards4.jpg"} 
+        width={300} 
+        alt="board"
+         height={300}
+         />
+        <Image
+          src={"/phone.png"}
+          width={300}
+          alt="phone"
+          height={300}
+          className="rounded-xl"
+        />
+        <Image
+         src={"/board1.jpg"}
+          width={300} 
+          alt="board"
+           height={300} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
